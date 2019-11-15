a24d29af41eb18d94153bad445631d0c18518163
Andrew Demers <andrew.demers91@gmail.com>
2019-11-14 19:15:53 -0600

    Implemented baron random tester

---
+++ projects/demersa/dominion/randomtestcard1.c
@@ -0,0 +1,22 @@
+/***********************************
+* Name: Andrew Demers
+* Date: 11/17/19
+* CS362 - 400
+* Assignment 4 - randomtestcard1.c
+* Description: This file contains a random tester for the baron card.
+***********************************/
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+void main() {
+	struct gameState G;
+	in
+}
